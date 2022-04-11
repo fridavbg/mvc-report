39299ee35ad5d2d09eda39479e40641f3a0be16e
fridavbg <fperssontech@gmail.com>
2022-04-11 22:19:56 +0200

    Search form init

---
+++ src/Controller/FormSearchController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FormSearchController extends AbstractController
+{
+    /**
+     * @Route("/form", name="form-search")
+     * /form/search varför försvinner CSSen??
+     */
+    public function search(Request $request): Response
+    {
+        $data = [
+            'search' => $request->query->get('search'),
+        ];
+
+        return $this->render('form/search.html.twig', $data);
+    }
+
+}
